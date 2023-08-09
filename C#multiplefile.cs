59c707631eb75266a9ca4490870417836309660d
rohitnegi1028 <86530226+rohitnegi1028@users.noreply.github.com>
2023-08-09 15:54:32 +0530

    Update C#multiplefile.cs

--- C#multiplefile.cs
+++ C#multiplefile.cs
@@ -32,3 +32,51 @@ public async Task<HttpResponseMessage> GetFilesResponse()
 
     return response;
 }
+
+
+
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+public class FileController : ControllerBase
+{
+    public IActionResult GetFilesResponse()
+    {
+        var files = new List<string>
+        {
+            "path_to_file1.txt",
+            "path_to_file2.txt",
+            // Add more file paths as needed
+        };
+
+        var memoryStream = new MemoryStream();
+
+        using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+        {
+            foreach (var filePath in files)
+            {
+                var entryName = Path.GetFileName(filePath);
+                var entry = archive.CreateEntry(entryName);
+
+                using (var entryStream = entry.Open())
+                using (var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    fileStream.CopyTo(entryStream);
+                }
+            }
+        }
+
+        memoryStream.Seek(0, SeekOrigin.Begin);
+
+        return new FileStreamResult(memoryStream, "application/zip")
+        {
+            FileDownloadName = "files.zip"
+        };
+    }
+}
+
+
+
+
+
+
