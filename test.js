e18e24949b1a3f79fd495f190bb2d7ba0457d1aa
rohitnegi1028 <86530226+rohitnegi1028@users.noreply.github.com>
2024-03-10 15:41:49 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,51 +1,66 @@
 const fetchDownStreamAndExpandTree = async (tree, movementData, locale, setServerErrors, setExpandedNodes, setSelectedRowIndex, showTerminatedRecords) => {
-    let currentNodes = tree;
-    let tempExpandedNodes = {};
-    let selectedRowString = '';
-    let isExpansionAchieved = false;
+  let nodes = [...tree]; // Copying the tree to avoid modifying the original parameter
+  let tempExpandedNodes = {};
+  let selectedRowString = '';
 
-    const processNode = async (currentNode, nodeIndex) => {
-        let modifiedNode = { ...currentNode };
-        if (!modifiedNode.children || modifiedNode.children.length === 0) {
-            modifiedNode.children = await fetchHierarchyDownStream(modifiedNode, locale, setServerErrors, showTerminatedRecords);
-        }
+  const checkForExpansion = (node, key) => {
+    return node.level === hierArr[key] && node.code === movementData[`movOrg${key}`];
+  };
 
-        tempExpandedNodes[nodeIndex] = {};
+  const fetchChildNodes = async (node) => {
+    const children = await fetchHierarchyDownStream(node, locale, setServerErrors, showTerminatedRecords);
+    node.children = children;
+  };
 
-        selectedRowString = selectedRowString ? `${selectedRowString}-${nodeIndex}` : `${nodeIndex}`;
+  const handleExpansion = (node, i, childIndex) => {
+    const updatedNodes = node.children;
+    tempExpandedNodes[i] = {};
+    tempExpandedNodes = tempExpandedNodes[i];
+    selectedRowString = selectedRowString ? `${selectedRowString}-${i}` : `${i}`;
 
-        const foundChildIndex = modifiedNode.children.findIndex((child) => child.code === movementData.movOrgCd && child.level === movementData.level);
-        if (foundChildIndex > -1) {
-            isExpansionAchieved = true;
-            selectedRowString = `${selectedRowString}-${foundChildIndex}`;
-        }
-
-        if (!isExpansionAchieved && modifiedNode.children) {
-            return Promise.all(modifiedNode.children.map(async (childNode, childIndex) => {
-                await processNode(childNode, childIndex);
-            }));
-        }
-    };
-
-    const processNodesSequentially = async () => {
-        const promises = [];
-        for (const [index, node] of currentNodes.entries()) {
-            promises.push(processNode(node, index));
-            if (isExpansionAchieved) {
-                break;
-            }
-        }
-        await Promise.all(promises);
-    };
-
-    for (const key of Object.keys(hierArr)) {
-        if (!isExpansionAchieved && movementData[`movOrg${key}`]) {
-            await processNodesSequentially();
-        }
+    if (childIndex > -1) {
+      selectedRowString = `${selectedRowString}-${childIndex}`;
+      setExpandedNodes(tempExpandedNodes);
+      setSelectedRowIndex(selectedRowString);
+      return true;
     }
 
-    const expandedNodesCopy = { ...tempExpandedNodes };
-    const selectedRowStringCopy = selectedRowString;
-    setExpandedNodes(expandedNodesCopy);
-    setSelectedRowIndex(selectedRowStringCopy);
+    return false;
+  };
+
+  const findExpansion = async (key) => {
+    for (const childNode of nodes) {
+      if (checkForExpansion(childNode, key)) {
+        if (!childNode.children || childNode.children.length === 0) {
+          await fetchChildNodes(childNode);
+        }
+
+        const childIndex = childNode.children.findIndex(child => child.code === movementData.movOrgCd && child.level === movementData.level);
+
+        if (handleExpansion(childNode, i, childIndex)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+
+  let foundExpansion = false;
+
+  for (const key of Object.keys(hierArr)) {
+    if (movementData[`movOrg${key}`]) {
+      let i = 0;
+
+      foundExpansion = await findExpansion(key);
+      
+      if (foundExpansion) {
+        break;
+      }
+    }
+  }
+
+  if (!foundExpansion) {
+    setExpandedNodes(tempExpandedNodes);
+    setSelectedRowIndex(selectedRowString);
+  }
 };
