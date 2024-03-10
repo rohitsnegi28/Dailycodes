956ae094544fbc7f9c7117b7cad461b6d4c65d6b
rohitnegi1028 <86530226+rohitnegi1028@users.noreply.github.com>
2024-03-10 15:22:30 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,47 +1,47 @@
-
 const fetchDownStreamAndExpandTree = async (tree, movementData, locale, setServerErrors, setExpandedNodes, setSelectedRowIndex, showTerminatedRecords) => {
     let currentNodes = tree;
     let tempExpandedNodes = {};
     let selectedRowString = '';
     let isExpansionAchieved = false;
 
-    const processNode = async (currentNode, index) => {
+    const processNode = async (currentNode, nodeIndex) => {
         let modifiedNode = { ...currentNode };
         if (!modifiedNode.children || modifiedNode.children.length === 0) {
             modifiedNode.children = await fetchHierarchyDownStream(modifiedNode, locale, setServerErrors, showTerminatedRecords);
         }
 
-        tempExpandedNodes[index] = {};
+        tempExpandedNodes[nodeIndex] = {};
 
-        selectedRowString = selectedRowString ? `${selectedRowString}-${index}` : `${index}`;
+        selectedRowString = selectedRowString ? `${selectedRowString}-${nodeIndex}` : `${nodeIndex}`;
 
-        const childIndex = modifiedNode.children.findIndex((child) => child.code === movementData.movOrgCd && child.level === movementData.level);
-        if (childIndex > -1) {
+        const foundChildIndex = modifiedNode.children.findIndex((child) => child.code === movementData.movOrgCd && child.level === movementData.level);
+        if (foundChildIndex > -1) {
             isExpansionAchieved = true;
-            selectedRowString = `${selectedRowString}-${childIndex}`;
+            selectedRowString = `${selectedRowString}-${foundChildIndex}`;
         }
 
         if (!isExpansionAchieved && modifiedNode.children) {
-            const promises = modifiedNode.children.map(async (childNode, childIndex) => {
+            return Promise.all(modifiedNode.children.map(async (childNode, childIndex) => {
                 await processNode(childNode, childIndex);
-            });
-            await Promise.all(promises);
+            }));
         }
     };
 
     const processNodesSequentially = async () => {
-        const promises = currentNodes.map(async (node, index) => {
-            await processNode(node, index);
-        });
+        const promises = [];
+        for (const [index, node] of currentNodes.entries()) {
+            promises.push(processNode(node, index));
+            if (isExpansionAchieved) {
+                break;
+            }
+        }
         await Promise.all(promises);
     };
 
     for (const key of Object.keys(hierArr)) {
-        if (isExpansionAchieved || !movementData[`movOrg${key}`]) {
-            continue;
+        if (!isExpansionAchieved && movementData[`movOrg${key}`]) {
+            await processNodesSequentially();
         }
-
-        await processNodesSequentially();
     }
 
     const expandedNodesCopy = { ...tempExpandedNodes };
