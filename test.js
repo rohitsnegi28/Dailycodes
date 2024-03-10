221be2e707f6067d443ba74f7c5da4ee7e0ebb44
rohitnegi1028 <86530226+rohitnegi1028@users.noreply.github.com>
2024-03-10 14:12:21 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,58 +1,61 @@
-const fetchDownStreamAndExpandTree = async (tree, movementData, locale, setServerErrors, setExpandedNodes, setSelectedRowIndex, showTerminatedRecords) => {
+const fetchDownStreamAndExpandTree = async (tree, movementData, locale, setServerErrors, setExpandedNodes, setSelectedRowIndex, showTerminatedRecords, reqObj) => {
     let nodes = tree;
     let tempExpandedNodes1 = {};
     let selectedRowString = '';
     let isExpansionAchieved = false;
 
-    const processNode = async (node, index) => {
-        if (!node.children || node.children.length === 0) {
-            const children = await fetchHierarchyDownStream(node, locale, setServerErrors, showTerminatedRecords);
-            node.children = children;
+    const processNode = async (currentNode, index) => {
+        if (!currentNode.children || currentNode.children.length === 0) {
+            const children = await fetchHierarchyDownStream(currentNode, locale, setServerErrors, showTerminatedRecords);
+            currentNode.children = children;
         }
         
-        nodes = node.children;
-        tempExpandedNodes1[index] = {};
-        tempExpandedNodes1 = tempExpandedNodes1[index];
+        nodes = currentNode.children;
+        const tempExpandedNodes = { ...tempExpandedNodes1 };
+        tempExpandedNodes[index] = {};
+        tempExpandedNodes1 = tempExpandedNodes;
         
         selectedRowString = selectedRowString ? `${selectedRowString}-${index}` : `${index}`;
 
-        const childIndex = node.children.findIndex((child) => child.code === movementData.movOrgCd && child.level === movementData.level);
+        const childIndex = currentNode.children.findIndex((child) => child.code === movementData.movOrgCd && child.level === movementData.level);
         if (childIndex > -1) {
             isExpansionAchieved = true;
             selectedRowString = `${selectedRowString}-${childIndex}`;
+            return;
         }
     };
 
+    const asyncTasks = [];
     for (const key of Object.keys(hierArr)) {
-        if (movementData[`movOrg${key}`]) {
-            let flag = true;
-            let i = 0;
-            
-            for (const node of nodes) {
-                if (node.code === movementData.movOrgCd && node.level === movementData.level) {
-                    selectedRowString = '0';
-                    isExpansionAchieved = true;
-                    break;
-                }
-                
-                if (node.level === hierArr[key] && node.code === movementData[`movOrg${key}`]) {
-                    await processNode(node, i);
-                    flag = false;
-                    break;
-                }
-                
-                if (!flag) {
-                    break;
-                }
-            }
-            i += 1;
+        if (isExpansionAchieved || !movementData[`movOrg${key}`]) {
+            continue;
         }
 
-        if (isExpansionAchieved) {
-            break;
+        let i = 0;
+        for (const node of nodes) {
+            if (isExpansionAchieved) {
+                break;
+            }
+
+            if (node.code === movementData.movOrgCd && node.level === movementData.level) {
+                selectedRowString = '0';
+                isExpansionAchieved = true;
+                break;
+            }
+            
+            if (node.level === hierArr[key] && node.code === movementData[`movOrg${key}`]) {
+                asyncTasks.push(processNode(node, i));
+                break;
+            }
+            
+            i += 1;
         }
     }
     
-    setExpandedNodes({});
-    setSelectedRowIndex(selectedRowString);
+    await Promise.all(asyncTasks);
+    
+    const expandedNodesCopy = { ...tempExpandedNodes1 };
+    const selectedRowStringCopy = selectedRowString;
+    setExpandedNodes(expandedNodesCopy);
+    setSelectedRowIndex(selectedRowStringCopy);
 };
